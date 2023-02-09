fe1abffb95f5da368884b70a0328d903dc473f45
Pedro del Rio <pedelriomarron@gmail.com>
2023-02-09 20:12:28 +0100

    Añadir al controllador la funcionalidad de editar datos mediante validación

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -100,6 +100,8 @@ class CustomerController extends Controller
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
         //
+         $customer->update($request->all());
+         return $customer;
     }
 
     /**
